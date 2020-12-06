402cd970c77f6732d2939badd954568ad21210a1
Francois Brodeur <francois.brodeur.87@gmail.com>
2020-12-05 19:04:38 -0500

    Day 5

--- day5/day5.go
+++ day5/day5.go
@@ -8,14 +8,49 @@ import (
 
 const (
 	errReadLines = "failed to read input as lines: %w"
+	maxRow       = 127
+	maxCol       = 7
+	multRow      = 8
+	two          = 2 // Sometimes I think the magic number linters is overboard ...
 )
+
+func findMiddle(min, max int, add, sub rune, chars string) int {
+	for _, char := range chars {
+		diff := (max - min + 1) / two
+		switch char {
+		case add:
+			min += diff
+		case sub:
+			max -= diff
+		}
+	}
+	return min
+}
+
+func seatID(row, col int) int {
+	return row*multRow + col
+}
+
+func parse(line string) int {
+	row := findMiddle(0, maxRow, 'B', 'F', line[:7])
+	col := findMiddle(0, maxCol, 'R', 'L', line[7:])
+	seat := seatID(row, col)
+	return seat
+}
 
 func Part1() (int, error) {
 	lines, err := util.ReadLines()
 	if err != nil {
 		return 0, fmt.Errorf(errReadLines, err)
 	}
-	return len(lines), nil
+	var highest int
+	for _, line := range lines {
+		seat := parse(line)
+		if seat > highest {
+			highest = seat
+		}
+	}
+	return highest, nil
 }
 
 func Part2() (int, error) {
@@ -23,5 +58,22 @@ func Part2() (int, error) {
 	if err != nil {
 		return 0, fmt.Errorf(errReadLines, err)
 	}
-	return len(lines), nil
+	seats := make(map[int]struct{})
+	for _, line := range lines {
+		seat := parse(line)
+		seats[seat] = struct{}{}
+	}
+	for row := 1; row < maxRow; row++ {
+		for col := 0; col < maxCol; col++ {
+			seat := seatID(row, col)
+			if _, ok := seats[seat]; !ok {
+				_, before := seats[seat-1]
+				_, after := seats[seat+1]
+				if before && after {
+					return seat, nil
+				}
+			}
+		}
+	}
+	return 0, nil
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -3,20 +3,20 @@ package day5_test
 import (
 	"testing"
 
-	"github.com/FrankBro/AdventOfCode2020/day4"
+	"github.com/FrankBro/AdventOfCode2020/day5"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPart1(t *testing.T) {
 	t.Parallel()
-	output, err := day4.Part1()
+	output, err := day5.Part1()
 	require.NoError(t, err)
-	require.EqualValues(t, 247, output)
+	require.EqualValues(t, 896, output)
 }
 
 func TestPart2(t *testing.T) {
 	t.Parallel()
-	output, err := day4.Part2()
+	output, err := day5.Part2()
 	require.NoError(t, err)
-	require.EqualValues(t, 145, output)
+	require.EqualValues(t, 659, output)
 }
